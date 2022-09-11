f45ef10966fb9d90362af4df56d80b390823686d
Ahmed Talaat <ahmedtal@banquemisr.com>
2022-09-11 17:43:09 +0200

    Initial Commit

---
+++ LandmarksSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LandmarksSwiftUI
+//
+//  Created by Ahmed Talaat on 11/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LandmarksSwiftUI/LandmarksSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksSwiftUIApp.swift
+//  LandmarksSwiftUI
+//
+//  Created by Ahmed Talaat on 11/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
